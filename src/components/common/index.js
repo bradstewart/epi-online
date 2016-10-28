3809a2af5e69dd38c75536b42d69300e39b8b0e0
Brad Stewart <me@bradstewart.co>
2016-10-28 14:02:02 -0500

    Removed unused components

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,22 +1,16 @@
 import AceEditor from './AceEditor'
-import LoadBar from './LoadBar'
 import LogOutput from './LogOutput'
-import Modal from './Modal'
 import Panel from './Panel'
 
 function install (Vue) {
   Vue.component('ace-editor', AceEditor)
-  Vue.component('load-bar', LoadBar)
   Vue.component('log-output', LogOutput)
-  Vue.component('modal', Modal)
   Vue.component('panel', Panel)
 }
 
 export {
   AceEditor,
-  LoadBar,
   LogOutput,
-  Modal,
   Panel,
 }
 
